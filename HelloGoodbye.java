1a8e8a5dbbbed48c0f08c1edebe62d770bb309dd
Surafelll <surafelwondimagegn01@gmail.com>
2022-06-02 04:20:57 -0700

    hello and goodbye

---
+++ HelloGoodbye.java
@@ -0,0 +1,11 @@
+/*import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;*/
+
+class HelloGoodbye {
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello Alejandra and Bahati.");
+        System.out.println("Goodbye Bahati and Alejandra."); 
+    }
+}

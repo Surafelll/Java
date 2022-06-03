8fa9c81bf8a2d4065fc2d776874ccc37c30572c5
Surafelll <surafelwondimagegn01@gmail.com>
2022-06-02 23:32:42 -0700

    hellogoodbyee created

---
+++ HelloGoodbyee.java
@@ -0,0 +1,11 @@
+/*import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;*/
+
+class HelloGoodbye {
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello Kevin and Bob.");
+        System.out.println("Goodbye Bob and Kevin."); 
+    }
+}
